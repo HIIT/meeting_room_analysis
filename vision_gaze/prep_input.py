69c68de2ab77c7c4cffa4e49768a4046d3c6cd72
Wang Tzu-Jui <wangt@whp-6.cs.helsinki.fi>
2016-08-24 12:28:25 +0300

    added MIT license to the header

--- vision_gaze/prep_input.py
+++ vision_gaze/prep_input.py
@@ -1,3 +1,24 @@
+'''
+  Copyright (c) 2016 University of Helsinki
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+'''
+
 import scipy
 import numpy as np
 from scipy import io, misc
@@ -54,7 +75,7 @@ class input_gazenet(object):
         self.input_image_resize = self.input_image_resize - self.places_mean
 
     def prep_eyes_image(self):
-        
+
         for head_box in self.head_boxes:
             top_left_x = int(head_box[0])
             top_left_y = int(head_box[1])
